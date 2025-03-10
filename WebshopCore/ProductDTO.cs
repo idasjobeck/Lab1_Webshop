a794a450a1cef90ba5e96a5db08f88aeb0a69bc8
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-03-10 12:19:59 +0100

    Added Genre, and made Series and NumberInSeries nullable, in the ProductDTO in the WebshopCore project.

--- WebshopCore/ProductDTO.cs
+++ WebshopCore/ProductDTO.cs
@@ -6,12 +6,13 @@
         public string ProductName { get; set; }
         public string Author { get; set; }
         public string Title { get; set; }
-        public string Series { get; set; }
-        public int NumberInSeries { get; set; }
+        public string? Series { get; set; }
+        public int? NumberInSeries { get; set; }
         public string Description { get; set; }
         public string ISBN { get; set; }
         public string Publisher { get; set; }
         public int PublishedYear { get; set; }
+        public string Genre { get; set; }
         public string ImageUrl { get; set; }
         public decimal Price { get; set; }
 
@@ -27,6 +28,7 @@
             ISBN = "ISBN";
             Publisher = "Publisher";
             PublishedYear = 1900;
+            Genre = "Genre";
             ImageUrl = "";
             Price = 0;
         }
