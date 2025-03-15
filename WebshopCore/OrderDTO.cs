b35064515007a2c2205a4798f012cbd90d57205f
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-03-15 19:19:29 +0100

    Updated Cart page to display items in the cart (saved in Local Storage) and a form for the user to fill in name and address. Cart page allows for items to be removed from the cart, and for the cart to be emptied. Also created UserWithAddressDTO and OrderDTO classes (in the WebshopCore project), both of which are used for the checkout functionality from the Cart page. Also created OrderConfirmation page which the user is taken to when clicking the checkout-button in the cart.

---
+++ WebshopCore/OrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopCore
+{
+    public class OrderDTO
+    {
+        public List<ProductDTO> ProductsOrdered { get; set; }
+        public string Currency { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal ShippingCost { get; set; }
+        public UserWithAddressDTO User { get; set; }
+    }
+}
---
+++ WebshopCore/UserWithAddressDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopCore
+{
+    public class UserWithAddressDTO
+    {
+        [Required(ErrorMessage = "The first name field is a required field.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The last name field is a required field.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The email field is a required field.")]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", ErrorMessage = "The email entered must be in a valid format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The address field is a required field.")]
+        public string Address1 { get; set; }
+        public string? Address2 { get; set; }
+
+        [Required(ErrorMessage = "The city field is a required field.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "The zip code field is a required field.")]
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "The country field is a required field.")]
+        public string Country { get; set; }
+    }
+}
