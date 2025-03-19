3bdd3a021fbd4e72dc336b3ec9b1f7e845552c02
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-03-19 17:20:13 +0100

    Migration having updated data model (Order.ShippingDate nullable and check constraint on Book.AvailableQty that it cannot be a negative number).

---
+++ WebshopBackend/Migrations/20250319161635_ModelRevisionShippingDateNullableAndBookAvailableQtyCheckConstraint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebshopBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevisionShippingDateNullableAndBookAvailableQtyCheckConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShippingDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Book_AvailableQty",
+                table: "Books",
+                sql: "AvailableQty >= 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Book_AvailableQty",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShippingDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
