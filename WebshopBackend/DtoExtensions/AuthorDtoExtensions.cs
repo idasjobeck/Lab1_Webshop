898336615d3ec4f6432ae9bf54d33dbb36f37d98
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-03-23 10:44:50 +0100

    Updated the AuthorDtoExtensions transforming a string to Author(s) and AuthorDto(s). Added some error handling, and a way to handle other separators than a comma in the case of multiple authors in a string. If three parts to the name (i.e. it includes initials) then initial is added in with the first name.

--- WebshopBackend/DtoExtensions/AuthorDtoExtensions.cs
+++ WebshopBackend/DtoExtensions/AuthorDtoExtensions.cs
@@ -14,11 +14,22 @@ namespace WebshopBackend.DtoExtensions
 
         public static List<Author> ToAuthors(this List<AuthorDto> authorDtos) => authorDtos.Select(ToAuthor).ToList();
 
-        // This assumes that the string is in the format "FirstName LastName"
-        // Would need to include some error handling if the string is not in this format
         public static Author ToAuthor(this string author)
         {
             var authorSplit = author.Split(' ');
+
+            if (authorSplit.Length < 2 || authorSplit.Length > 3)
+                throw new ArgumentException(
+                    "Author string must be in the format 'FirstName LastName' or 'FirstName Initial LastName");
+
+            if (authorSplit.Length == 3)
+                return new Author
+                {
+                    Id = 0,
+                    FirstName = $"{authorSplit[0]} {authorSplit[1]}",
+                    LastName = authorSplit[2]
+                };
+
             return new Author
             {
                 Id = 0,
@@ -27,9 +38,13 @@ namespace WebshopBackend.DtoExtensions
             };
         }
 
-        // This assumes that the string of authors is in the format "FirstName LastName, FirstName LastName, ..."
-        // Would need to include some error handling if the string is not in this format, and to provide a way to handle other separators
-        public static List<Author> ToAuthors(this string authors) => authors.Split(", ").Select(ToAuthor).ToList();
+        public static List<Author> ToAuthors(this string authors)
+        {
+            if (authors.Contains(';'))
+                return authors.Split("; ").Select(ToAuthor).ToList();
+
+            return authors.Split(", ").Select(ToAuthor).ToList();
+        }
 
         public static AuthorDto ToAuthorDto(this Author author) => new AuthorDto
         {
@@ -39,11 +54,21 @@ namespace WebshopBackend.DtoExtensions
 
         public static List<AuthorDto> ToAuthorDtos(this List<Author> authors) => authors.Select(ToAuthorDto).ToList();
 
-        // This assumes that the string is in the format "FirstName LastName"
-        // Would need to include some error handling if the string is not in this format
         public static AuthorDto ToAuthorDto(this string author)
         {
             var authorSplit = author.Split(' ');
+
+            if (authorSplit.Length < 2 || authorSplit.Length > 3)
+                throw new ArgumentException(
+                    "Author string must be in the format 'FirstName LastName' or 'FirstName Initial LastName");
+
+            if (authorSplit.Length == 3)
+                return new AuthorDto
+                {
+                    FirstName = $"{authorSplit[0]} {authorSplit[1]}",
+                    LastName = authorSplit[2]
+                };
+
             return new AuthorDto
             {
                 FirstName = authorSplit[0],
@@ -51,9 +76,13 @@ namespace WebshopBackend.DtoExtensions
             };
         }
 
-        // This assumes that the string of authors is in the format "FirstName LastName, FirstName LastName, ..."
-        // Would need to include some error handling if the string is not in this format, and to provide a way to handle other separators
-        public static List<AuthorDto> ToAuthorDtos(this string authors) => authors.Split(", ").Select(ToAuthorDto).ToList();
+        public static List<AuthorDto> ToAuthorDtos(this string authors)
+        {
+            if (authors.Contains(';'))
+                return authors.Split("; ").Select(ToAuthorDto).ToList();
+
+            return authors.Split(", ").Select(ToAuthorDto).ToList();
+        }
 
         public static string ToAuthorsAsString(this List<Author> authors) => 
             string.Join(", ", authors.Select(a => $"{a.FirstName} {a.LastName}"));
