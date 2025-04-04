a54b5a17604524db89d852f582bceb03e3ece696
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-04-04 11:20:33 +0200

    Added CartDto in WebshopCore and extension methods to transform from Cart to CartDto (and vice versa) in WebshopBackend.

---
+++ WebshopBackend/DtoExtensions/CartDtoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using WebshopBackend.Models;
+using WebshopCore.Dtos;
+
+namespace WebshopBackend.DtoExtensions
+{
+    public static class CartDtoExtensions
+    {
+        public static Cart ToCart(this CartDto cartDto) => new Cart
+        {
+            Id = 0,
+            UserId = cartDto.UserId,
+            JsonCart = cartDto.JsonCart
+        };
+
+        public static CartDto ToCartDto(this Cart cart) => new CartDto
+        {
+            UserId = cart.UserId,
+            JsonCart = cart.JsonCart
+        };
+    }
+}
